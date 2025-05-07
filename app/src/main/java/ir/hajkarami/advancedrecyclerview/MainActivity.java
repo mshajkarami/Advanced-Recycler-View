7cd999afeb733ab2c166e93663c3ea95a2dbbaeb
mshajkarami <mshajkarami.dev@gmail.com>
2025-05-07 09:46:17 +0330

    Add Button : btn 1 , btn 2

--- app/src/main/java/ir/hajkarami/advancedrecyclerview/MainActivity.java
+++ app/src/main/java/ir/hajkarami/advancedrecyclerview/MainActivity.java
@@ -1,6 +1,8 @@
 package ir.hajkarami.advancedrecyclerview;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn1 , btn2 ;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +25,13 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
+
+
 }
